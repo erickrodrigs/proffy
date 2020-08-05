88dc4d11c76a828465bda2f214e94b00613bb46d
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2020-08-05 17:51:29 -0300

    start TeacherForm structure and feat Input component

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+
+import './styles.css'
 
 function TeacherForm() {
   return (
@@ -7,6 +10,15 @@ function TeacherForm() {
       <PageHeader 
         title="Que incrível que você quer dar aulas." 
         description="O primeiro passo é preencher esse formulário de inscrição" />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+          <Input name="name" label="Nome completo" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="Whatsapp" />
+        </fieldset>
+      </main>
     </div>
   );
 }
