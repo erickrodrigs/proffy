27c51d199602271e5f81499c796c725a74a3f703
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2020-08-06 01:44:35 -0300

    feat creating new connection when student contacts teacher

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
 export interface Teacher {
   id: number;
@@ -19,6 +20,12 @@ interface TeacherItemProps {
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
@@ -36,7 +43,7 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
           Preço/hora
           <strong>R$ { teacher.cost }</strong>
         </p>
-        <a href={ `https://wa.me/${ teacher.whatsapp }` } target="blank">
+        <a onClick={ createNewConnection } href={ `https://wa.me/${ teacher.whatsapp }` } target="blank">
           <img src={ whatsappIcon } alt="Whatsapp" />
           Entrar em contato
         </a>
