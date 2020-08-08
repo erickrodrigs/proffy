9b6c69214b28b056d88f62cf46c94bbe6dc647c1
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2020-08-07 23:04:59 -0300

    feat filter form

--- mobile/src/pages/TeacherList/styles.ts
+++ mobile/src/pages/TeacherList/styles.ts
@@ -6,7 +6,44 @@ const styles = StyleSheet.create({
     backgroundColor: '#F0F0F7'
   },
   teacherList: {
-    marginTop: -60
+    marginTop: -40
+  },
+  searchForm: {
+    marginBottom: 24,
+  },
+  label: {
+    color: '#D4C2FF',
+    fontFamily: 'Poppins_400Regular'
+  },
+  input: {
+    height: 54,
+    backgroundColor: '#FFF',
+    borderRadius: 8,
+    justifyContent: 'center',
+    paddingHorizontal: 16,
+    marginTop: 4,
+    marginBottom: 16
+  },
+  inputGroup: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  inputBlock: {
+    width: '45%'
+  },
+  submitButton: {
+    backgroundColor: '#04D361',
+    height: 56,
+    borderRadius: 8,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 4
+  },
+  submitButtonText: {
+    color: '#FFF',
+    fontFamily: 'Archivo_700Bold',
+    fontSize: 16
   }
 });
 
